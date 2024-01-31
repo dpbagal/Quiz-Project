eabae18105327126c6008d0fab191182900e42d9
dbagal <bagaldnyaneshwar226@gmail.com>
2024-01-31 22:23:55 +0530

    Updating form dnyaneshwar

--- Class-Quiz/src/com/quizapp/velocity/exam/TestForGit.java
+++ Class-Quiz/src/com/quizapp/velocity/exam/TestForGit.java
@@ -3,5 +3,6 @@ package com.quizapp.velocity.exam;
 public class TestForGit {
 public static void main(String[] args) {
 	System.out.println("Checking file uploaded on git or not");
+	System.out.println("Updating from Dnyaneshwar bagal");
 }
 }
